9f7a0dd7bf6c36e8071304ab416cae2cceda0aab
Sam Towers <jamisbrillx@gmail.com>
2021-05-05 13:26:39 +0100

    Add files via upload

---
+++ CreateNSG.ps1
@@ -0,0 +1,12 @@
+﻿#Creating config Network sec group 
+Connect-AzAccount
+# Vars 
+$RG='TestRG'
+$Location='UK South'
+$NSGname = 'AllowHTTP'
+#Creatiung the NSG 
+New-AzNetworkSecurityGroup -Name $NSGname -ResourceGroupName $RG -Location $Location 
+#Create a rule :) 
+#New-AzNetworkSecurityGroup -Name $NSGname -ResourceGroupName TestRG -Location $Location 
+New-AzNetworkSecurityRuleConfig -Name $NSGname -Description "ALLOW THE HTTP :)" -Access Allow -Protocol Tcp -Direction Inbound -Priority 101 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80 # allow port 80 = http 
+  
