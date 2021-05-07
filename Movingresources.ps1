d75a2b2400c7dea177616c072b1f46569b41565a
Sam Towers <jamisbrillx@gmail.com>
2021-05-07 12:31:30 +0100

    Delete Movingresources.ps1

--- Movingresources.ps1
+++
@@ -1,8 +0,0 @@
-#Moving Resources Across resource groups
-Get-AzResource -ODataQuery "`$filter=resourcegroup eq 'TestRG'" | Format-Table -Property ResourceID 
-#Move the resource 
-Move-AzResource -DestinationResourceGroupName "NewTestRG" -ResourceId "/subscriptions/Some ID Here /resourceGroups/TestRG/providers/Microsoft.Network/virtualNetworks/TestRG-vnet"  # Put a resource id here 
-#Check on the resource we just moved
-Get-AzResource -ODataQuery "`$filter=resourcegroup eq 'NewTestRG'" | Format-Table -Property ResourceID 
-
-
