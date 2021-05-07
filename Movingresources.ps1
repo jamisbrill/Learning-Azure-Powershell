d05391c299068248841d48de00595f6e77f0d8c4
Sam Towers <jamisbrillx@gmail.com>
2021-05-07 12:29:17 +0100

    Update Movingresources.ps1

--- Movingresources.ps1
+++ Movingresources.ps1
@@ -1,7 +1,7 @@
 #Moving Resources Across resource groups
 Get-AzResource -ODataQuery "`$filter=resourcegroup eq 'TestRG'" | Format-Table -Property ResourceID 
 #Move the resource 
-Move-AzResource -DestinationResourceGroupName "NewTestRG" -ResourceId "/subscriptions/7816526c-c2fe-4322-be3e-445c0d12f396/resourceGroups/TestRG/providers/Microsoft.Network/virtualNetworks/TestRG-vnet"  # Put a resource id here 
+Move-AzResource -DestinationResourceGroupName "NewTestRG" -ResourceId "/subscriptions/Some ID Here /resourceGroups/TestRG/providers/Microsoft.Network/virtualNetworks/TestRG-vnet"  # Put a resource id here 
 #Check on the resource we just moved
 Get-AzResource -ODataQuery "`$filter=resourcegroup eq 'NewTestRG'" | Format-Table -Property ResourceID 
 
