0b403b41474b6ee4946660ef1d9076b4bf353dfe
Sam Towers <jamisbrillx@gmail.com>
2021-06-08 10:50:20 +0100

    Update Create-Storage-Account.ps1

--- Create-Storage-Account.ps1
+++ Create-Storage-Account.ps1
@@ -1,7 +1,10 @@
 Get-AzResourceGroup TestRG #check resourceGroupExists 
 #create storage account 
  New-AzStorageAccount -name filestorestorage5 -ResourceGroupName TestRG -SkuName Standard_LRS -location "Uk South" -kind Storagev2
-#delete storage account
+  
+ #Adjust to ZRS
+Set-AzStorageAccount -ResourceGroupName TestRG  -AccountName filestorestorage5  -SkuName Standard_GRS
 
+#delete storage account
 Get-AzStorageAccount #check storage accountexists before deleting
 Remove-AzStorageAccount -name filestorestorage5 -ResourceGroupName TestRG
