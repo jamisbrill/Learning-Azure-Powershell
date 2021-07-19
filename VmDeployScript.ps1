66d27e5c25480a32a5583f37e12f868ff55d0e11
Sam Towers <jamisbrillx@gmail.com>
2021-07-19 15:41:10 +0100

    Create VmDeployScript.ps1

---
+++ VmDeployScript.ps1
@@ -0,0 +1,15 @@
+$resourceGroupName = Read-Host -Prompt "Mc"
+$location = Read-Host -Prompt "centralus"
+$adminUsername = Read-Host -Prompt "jam"
+$adminPassword = Read-Host -Prompt "password" -AsSecureString  #Please change this .... (Probably wont let you use this pass) 
+#$dnsLabelPrefix = Read-Host -Prompt "Enter an unique DNS name for the public IP"
+
+New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateUri "https://raw.githubusercontent.com/jamisbrill/Learning-Azure-Powershell/main/templates/SimpleVM1.json" `
+    -adminUsername $adminUsername `
+    -adminPassword $adminPassword `
+    -dnsLabelPrefix $dnsLabelPrefix
+
+ (Get-AzVm -ResourceGroupName $resourceGroupName).name
