f3722306d7f04d9e8ef0786f3101505bf9b6e9d5
Sam Towers <jamisbrillx@gmail.com>
2021-08-19 15:58:41 +0100

    Update Schedule.ps1

--- Automation/Schedule.ps1
+++ Automation/Schedule.ps1
@@ -4,15 +4,9 @@ New-AzureAutomationAccount -ResourceGroupName StandardAutomationRG -Name AzureAu
 $TimeZone = ([System.TimeZoneInfo]::Local).Id
 
 New-AzureAutomationRunbook -Name RetrieveData_SendMail -Type PowerShell -ResourceGroupName StandardAutomationRG -AutomationAccountName AzureAutomationAccount 
-
-
 Import-AzureAutomationRunbook -Name "Start VM" -Path "D:\GetData.ps1" -Type PowerShell -ResourceGroupName StandardAutomationRG -AutomationAccountName AzureAutomationAccount –Force
 #see if we can add a URL Path to github instead  #This will append the script to the the run book 
-
-
 Publish-AzureAutomationRunbook -AutomationAccountName AzureAutomationAccount -ResourceGroupName StandardAutomationRG -Name "Start VM" #publish the automation script. 
-
-
 New-AzAutomationSchedule -AutomationAccountName "StandardAutomation" -Name "Schedule01" -StartTime "23:00" -OneTime -ResourceGroupName "new" -TimeZone $TimeZone
 
 #Stub please expand. 
