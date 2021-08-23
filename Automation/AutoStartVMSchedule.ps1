ee4bf5da0a260e7e2e31343e58cc3b8af2ee8359
Sam Towers <jamisbrillx@gmail.com>
2021-08-23 13:45:10 +0100

    Update and rename AutoStopVMSchedule.ps1 to AutoStartVMSchedule.ps1

--- Automation/AutoStartVMSchedule.ps1
+++ Automation/AutoStartVMSchedule.ps1
@@ -1,15 +1,22 @@
+#Setup vars for az automation
+$AzureAutomationAccountName = AzureAutomationAccount
+$runbookName = StartVM
+$scheduleName = StartVmSchedule
+
 #Create an Azure Automation runbook
 New-AzResourceGroup -Name 'StandardAutomationRG' -Location 'Uksouth' -Tag @{'Env'='Test'} #create a rg to store the automation stuff in 
 New-AzAutomationAccount -ResourceGroupName StandardAutomationRG -Name AzureAutomationAccount -Location "Uk South" #create a automation account 
 $TimeZone = ([System.TimeZoneInfo]::Local).Id #get the local timezone 
 
 New-AzAutomationRunbook -Name RetrieveData_SendMail -Type PowerShell -ResourceGroupName StandardAutomationRG -AutomationAccountName AzureAutomationAccount 
-Import-AZAutomationRunbook -Name "Start VM" -Path "C:\Users\jamis\Documents\Azure\Powershell\StartVm.ps1" -Type PowerShell -ResourceGroupName StandardAutomationRG -AutomationAccountName AzureAutomationAccount –Force
+Import-AZAutomationRunbook -Name "StartVM" -Path "C:\Users\jamis\Documents\Azure\Powershell\StartVm.ps1" -Type PowerShell -ResourceGroupName StandardAutomationRG -AutomationAccountName AzureAutomationAccount –Force
 
 #see if we can add a URL Path to github instead  #This will append the script to the the run book 
 Publish-AzAutomationRunbook -AutomationAccountName AzureAutomationAccount -ResourceGroupName StandardAutomationRG -Name "Start VM" #publish the automation script. 
-New-AzAutomationSchedule -AutomationAccountName "AzureAutomationAccount" -Name "Schedule01" -StartTime "22:40" -ResourceGroupName "StandardAutomationRG" -TimeZone $TimeZone -DayInterval 1
+New-AzAutomationSchedule -AutomationAccountName "AzureAutomationAccount" -Name "StartVmSchedule" -StartTime "6:40" -ResourceGroupName "StandardAutomationRG" -TimeZone $TimeZone -DayInterval 1
 #link to the schedule to the runbook 
+Register-AzureAutomationScheduledRunbook –AutomationAccountName $automationAccountName –Name $runbookName –ScheduleName $scheduleName #–Parameters $params
+#Not sure what the params are for.... 
 
 #Stub please expand. 
 # adds a schedule to the automation account/ Requires a automation account ... Needs linking to a run book/Script etc 
