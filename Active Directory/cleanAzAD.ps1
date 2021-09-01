5d7ee7433eea64a7eb167aaa65145de06973357e
Sam Towers <jamisbrillx@gmail.com>
2021-09-01 21:32:40 +0100

    Create cleanAzAD.ps1

---
+++ Active Directory/cleanAzAD.ps1
@@ -0,0 +1,7 @@
+Connect-AzureAD
+[system.io.directory]::CreateDirectory(“D:\ADSample\”)
+
+Get-AzureADUser -All $true | Export-Csv D:\ADSample\AllUsers.csv
+Import-CSV D:\ADSample\AllUsers.csv | Foreach-Object { Remove-AzureADUser -ObjectId $_.UserPrincipalName }
+
+#you may need adjust drive and add the folder 
