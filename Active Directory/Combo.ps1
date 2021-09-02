f63042993c61af5b387486fafeb8273d9ef5b90b
Sam Towers <jamisbrillx@gmail.com>
2021-09-02 10:42:39 +0100

    Create Combo.ps1

---
+++ Active Directory/Combo.ps1
@@ -0,0 +1,23 @@
+#For creating singular users and assigning them to a newly created group // Not the most convenient script.
+#Variables
+$password = Read-Host "Please enter the password for the user"
+$username = Read-Host "please enter the username"
+$mailNickName = $username 
+$domain = Read-Host "Enter the domain of the Az Ad Instance" 
+$at = "@" # part of the concatenation of strings 
+$userPrincipalName = $username+$at+$domain #  concat strings
+$groupDisplayName = Read-Host "Please enter te groups name "  # variable for group name
+
+New-AzADGroup -DisplayName $groupDisplayName -MailNickname $groupDisplayName #create a new group 
+Add-AzADGroupMember -MemberUserPrincipalName $userPrincipalName -TargetGroupDisplayName $groupDisplayName  #add the user you just created and add them to the group 
+
+
+
+
+
+
+Connect-AzureAD
+$SecureStringPassword = ConvertTo-SecureString -String $password -AsPlainText -Force  #Example password 
+New-AzADUser -DisplayName $username -UserPrincipalName $userPrincipalName -Password $SecureStringPassword -MailNickname $mailNickName
+
+
