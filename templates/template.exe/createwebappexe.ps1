88df0e0b44e29a41ec7a74b7c388fb05ed2e8daf
Sam Towers <jamisbrillx@gmail.com>
2021-09-03 14:49:14 +0100

    Create createwebappexe.ps1

---
+++ templates/template.exe/createwebappexe.ps1
@@ -0,0 +1,17 @@
+
+
+$resourceGroupName = "myfirstswadeployRG"
+
+New-AzResourceGroup `
+  -Name $resourceGroupName `
+  -Location "Central US"
+
+$templateFile = Read-Host -Prompt "Enter the template file path and file name"
+$templateparameterFile = Read-Host -Prompt "Enter the template parameter file path and file name"
+
+New-AzResourceGroupDeployment `
+  -Name DeployLocalTemplate `
+  -ResourceGroupName $resourceGroupName `
+  -TemplateFile $templateFile `
+  -TemplateParameterFile $templateparameterfile `
+  -verbose
