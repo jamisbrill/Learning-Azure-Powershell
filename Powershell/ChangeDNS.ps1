e766aed31c9e0f6c17f1b5714f264f71e3e0f64d
Sam Towers <jamisbrillx@gmail.com>
2021-09-12 15:13:35 +0100

    Create ChangeDNS.ps1

---
+++ Powershell/ChangeDNS.ps1
@@ -0,0 +1,3 @@
+#Set-DNSClientServerAddress –interfaceIndex 12 –ServerAddresses (“192.168.1.50”,”192.168.1.1”)
+Set-DNSClientServerAddress –interfaceIndex 8 –ServerAddresses (“192.168.1.1”)
+Get-NetIPInterface #-InterfaceAlias
