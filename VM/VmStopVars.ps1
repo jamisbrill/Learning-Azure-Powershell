52fda25beb24482f89475eb5d89a1b64538f0791
Sam Towers <jamisbrillx@gmail.com>
2021-09-15 17:06:28 +0100

    Update VmStopVars.ps1

--- VM/VmStopVars.ps1
+++ VM/VmStopVars.ps1
@@ -1,18 +1,13 @@
- 
- 
- $checkfilebool = Test-Path -Path .\VMNameVars.txt -PathType Leaf
+ # Vm stop script to store the valye 
+ $checkfilebool = Test-Path -Path .\VMNameVars.txt -PathType Leaf #true or false whether file exists or not. 
 
- 
-if ( !$checkfilebool )
+if ( !$checkfilebool ) #if file has not been created , create it
 {
- $VMName = Read-Host "Please enter the virtual machine name"
+ $VMName = Read-Host "Please enter the virtual machine name" # prompt the user to enter the name of the virtual machine 
 }
  
- echo $VMName| Out-File -FilePath .\VMNameVars.txt
- Get-Content -Path .\VMNameVars.txt # output the virtual machine name to a file to store for later 
+echo $VMName| Out-File -FilePath .\VMNameVars.txt  # put the virtual machine name in a file to save for later 
+Get-Content -Path .\VMNameVars.txt # output the virtual machine name to a file to store for later 
 
+Stop-AzVM -ResourceGroupName "ResourceGroup" -Name $VMName #Deollocate the vm  
  
- Stop-AzVM -ResourceGroupName "ResourceGroup" -Name $VMName #Deollocate the vm  
- 
- 
- #test if loop etc 15/09/21 
