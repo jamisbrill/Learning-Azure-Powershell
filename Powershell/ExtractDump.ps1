a7b04c5d21dc61728d79910429b890f632a15b38
Sam Towers <jamisbrillx@gmail.com>
2021-09-17 12:02:55 +0100

    Create ExtractDump.ps1

---
+++ Powershell/ExtractDump.ps1
@@ -0,0 +1,17 @@
+#Script to go thru archives and extract the data 
+
+Get-ChildItem  "D:\UserFiles\Games\Xbox Games" -Filter *.txt -Recurse | % { $_.FullName }
+
+cd "D:\UserFiles\Games\Xbox Games"
+$fileint = (Get-ChildItem | Measure-Object).Count # count the number of files and set that as the integer to use for the for loop 
+$filepath = ""
+
+
+for ($fileint; <Condition>; fileint++)
+{
+  
+  Expand-Archive 
+
+  
+  
+}
