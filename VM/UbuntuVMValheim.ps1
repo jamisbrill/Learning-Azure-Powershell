52ef2c7db249d250ae40af201f2d43daab57364f
Sam Towers <jamisbrillx@gmail.com>
2022-04-25 12:10:19 +0100

    Create UbuntuVMValheim.ps1

---
+++ VM/UbuntuVMValheim.ps1
@@ -0,0 +1,21 @@
+New-AzResourceGroup -Name "LinuxVmRG" -Location "EastUS"
+
+
+New-AzVm `
+    -ResourceGroupName "LinuxVmRG" `
+    -Name "UbuntuVM" `
+    -Location "Uk South" `
+    -Image UbuntuLTS `
+    -size Standard_B2s `
+    -PublicIpAddressName myPubIP `
+    -OpenPorts 80,22 `
+    -GenerateSshKey `
+    -SshKeyName mySSHKey
+    
+Get-AzPublicIpAddress -ResourceGroupName "LinuxVmRG" | Select "IpAddress"
+
+
+#Private key is saved to /home/user/.ssh/1234567891
+#Open SSH Client 
+#ssh -i ~/.ssh/1234567891 azureuser@10.111.12.123
+#https://docs.microsoft.com/en-us/azure/virtual-machines/linux/quick-create-powershell
