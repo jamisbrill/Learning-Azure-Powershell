1c37af5bfb0cb2235cdf891014cf3403434974f4
Sam Towers <jamisbrillx@gmail.com>
2022-10-26 08:59:00 +0100

    Create ManagingM65Groups.ps1

---
+++ Teams/ManagingM65Groups.ps1
@@ -0,0 +1,5 @@
+Connect-AzureAD
+Set-UnifiedGroup -DisplayName "Tech Department" -RequireSenderAuthenticationEnabled $false
+#to receive mail from unauthenticated (external) senders
+
+#Comebck to this. 
