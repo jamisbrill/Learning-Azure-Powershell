7f6532c592a98dfbc81cea26645531bfaa3a7c4c
Sam Towers <jamisbrillx@gmail.com>
2022-11-14 08:28:05 +0000

    Create ModifyTeam.ps1

---
+++ Teams/ModifyTeam.ps1
@@ -0,0 +1,2 @@
+Get-Team -DisplayName "Technical Support" | Set-Team -Description "This is the team for the Fixing all the technical problems that arise externally & internally" -Visibility Public 
+
