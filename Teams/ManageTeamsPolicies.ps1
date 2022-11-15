98bf13d9cdf153b0b1b8f2bb4b959738c13058fd
Sam Towers <jamisbrillx@gmail.com>
2022-11-15 12:24:10 +0000

    Create ManageTeamsPolicies.ps1

---
+++ Teams/ManageTeamsPolicies.ps1
@@ -0,0 +1,7 @@
+New-CsTeamsChannelsPolicy -Identity "IT-Department" -Description "All members of the IT-Department" -AllowPrivateChannelCreation:$false
+
+#New-CsTeamsChannelsPolicy
+#Set-CsTeamsChannelsPolicy
+#Get-CsTeamsChannelsPolicy
+#Grant-CsTeamsChannelsPolicy
+#Remove-CsTeamsChannelsPolicy
