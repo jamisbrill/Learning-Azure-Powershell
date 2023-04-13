79599b9abb1dacc48389e260cb095ae605bf6b0c
Sam Towers <jamisbrillx@gmail.com>
2023-04-13 08:43:50 +0100

    Update DeleteTeam.ps1

--- Teams/DeleteTeam.ps1
+++ Teams/DeleteTeam.ps1
@@ -1,4 +1,4 @@
-#Permanently delete a team
+#Permanently delete a team, has to be deleted first. 
 Get-AzureADMSDeletedGroup
 Remove-AzureADMSDeletedDirectoryObject -Id <objectId>
 #replace objectid with id given in first cmdlet 
